26b5756464f96feebafe9063c33965aaadac4442
Benjamin Abendroth <braph93@gmx.de>
2024-08-16 10:11:18 +0200

    Fixed wrong indendation in zsh_helpers.py

--- argparse_shell_complete/zsh_helpers.py
+++ argparse_shell_complete/zsh_helpers.py
@@ -7,9 +7,9 @@ local FUNC='get_positional'
 local CONTAINS="${FUNC}_contains"
 
 $CONTAINS() {
-local ARG KEY="$1"; shift
-for ARG; do [[ "$KEY" == "$ARG" ]] && return 0; done
-return 1
+  local ARG KEY="$1"; shift
+  for ARG; do [[ "$KEY" == "$ARG" ]] && return 0; done
+  return 1
 }
 
 local IFS=','
@@ -46,105 +46,105 @@ local -a HAVING_OPTIONS
 
 local ARGI=2 # ARG[1] is program name
 while [[ $ARGI -le $# ]]; do
-local ARG="${@[$ARGI]}"
-local HAVE_TRAILING_ARG=$(test $ARGI -lt $# && echo true || echo false)
-
-case "$ARG" in
-  (-)
-    POSITIONALS+=(-);;
-  (--)
-    for ARGI in $(seq $((ARGI + 1)) $#); do
-      POSITIONALS+=("${@[$ARGI]}")
-    done
-    break;;
-  (--*)
-    for OPTION in $LONG_OPTS_WITH_ARG $LONG_OPTS_WITHOUT_ARG $LONG_OPTS_WITH_OPTIONAL_ARG; do
-      if [[ "$ARG" == "$OPTION="* ]]; then
-        HAVING_OPTIONS+=("$OPTION")
-        break
-      elif [[ "$ARG" == "$OPTION" ]]; then
-        if $CONTAINS "$OPTION" "${LONG_OPTS_WITH_ARG[@]}"; then
-          if $HAVE_TRAILING_ARG; then
-            HAVING_OPTIONS+=("$OPTION")
-            (( ARGI++ ))
-          fi
-        else
-          HAVING_OPTIONS+=("$OPTION")
-        fi
-        break
-      fi
-    done;;
-  (-*)
-    local HAVE_MATCH=false
-
-    for OPTION in $OLD_OPTS_WITH_ARG $OLD_OPTS_WITHOUT_ARG $OLD_OPTS_WITH_OPTIONAL_ARG; do
-      if [[ "$ARG" == "$OPTION="* ]]; then
-        HAVING_OPTIONS+=("$OPTION")
-        HAVE_MATCH=true
-        break
-      elif [[ "$ARG" == "$OPTION" ]]; then
-        if $CONTAINS "$OPTION" "${OLD_OPTS_WITH_ARG[@]}"; then
-          if $HAVE_TRAILING_ARG; then
-            HAVING_OPTIONS+=("$OPTION")
-            (( ARGI++ ))
-          fi
-        else
-          HAVING_OPTIONS+=("$OPTION")
-        fi
-
-        HAVE_MATCH=true
-        break
-      fi
-    done
-
-    if ! $HAVE_MATCH; then
-      local ARG_LENGTH=${#ARG}
-      local I=1
-      local IS_END=false
-      while ! $IS_END && test $I -lt $ARG_LENGTH; do
-        local ARG_CHAR="${ARG:$I:1}"
-        local HAVE_TRAILING_CHARS=$(test $((I+1)) -lt $ARG_LENGTH && echo true || echo false)
-
-        for OPTION in $SHORT_OPTS_WITH_ARG $SHORT_OPTS_WITHOUT_ARG $SHORT_OPTS_WITH_OPTIONAL_ARG; do
-          local OPTION_CHAR="${OPTION:1:1}"
-
-          if test "$ARG_CHAR" = "$OPTION_CHAR"; then
-            if $CONTAINS "$OPTION" "${SHORT_OPTS_WITH_ARG[@]}"; then
-              if $HAVE_TRAILING_CHARS; then
-                HAVING_OPTIONS+=("$OPTION")
-                IS_END=true
-              elif $HAVE_TRAILING_ARG; then
-                HAVING_OPTIONS+=("$OPTION")
-                (( ARGI++ ))
-                IS_END=true
-              fi
-            elif $CONTAINS "$OPTION" "${SHORT_OPTS_WITH_OPTIONAL_ARG[@]}"; then
-              HAVING_OPTIONS+=("$OPTION")
-
-              if $HAVE_TRAILING_CHARS; then
-                IS_END=true
-              fi
-            else
-              HAVING_OPTIONS+=("$OPTION")
-            fi
-
-            break
-          fi
-        done
-
-        (( I++ ))
+  local ARG="${@[$ARGI]}"
+  local HAVE_TRAILING_ARG=$(test $ARGI -lt $# && echo true || echo false)
+  
+  case "$ARG" in
+    (-)
+      POSITIONALS+=(-);;
+    (--)
+      for ARGI in $(seq $((ARGI + 1)) $#); do
+        POSITIONALS+=("${@[$ARGI]}")
       done
-    fi;;
-  (*)
-    POSITIONALS+=("$ARG");;
-esac
-
-(( ARGI++ ))
+      break;;
+    (--*)
+      for OPTION in $LONG_OPTS_WITH_ARG $LONG_OPTS_WITHOUT_ARG $LONG_OPTS_WITH_OPTIONAL_ARG; do
+        if [[ "$ARG" == "$OPTION="* ]]; then
+          HAVING_OPTIONS+=("$OPTION")
+          break
+        elif [[ "$ARG" == "$OPTION" ]]; then
+          if $CONTAINS "$OPTION" "${LONG_OPTS_WITH_ARG[@]}"; then
+            if $HAVE_TRAILING_ARG; then
+              HAVING_OPTIONS+=("$OPTION")
+              (( ARGI++ ))
+            fi
+          else
+            HAVING_OPTIONS+=("$OPTION")
+          fi
+          break
+        fi
+      done;;
+    (-*)
+      local HAVE_MATCH=false
+  
+      for OPTION in $OLD_OPTS_WITH_ARG $OLD_OPTS_WITHOUT_ARG $OLD_OPTS_WITH_OPTIONAL_ARG; do
+        if [[ "$ARG" == "$OPTION="* ]]; then
+          HAVING_OPTIONS+=("$OPTION")
+          HAVE_MATCH=true
+          break
+        elif [[ "$ARG" == "$OPTION" ]]; then
+          if $CONTAINS "$OPTION" "${OLD_OPTS_WITH_ARG[@]}"; then
+            if $HAVE_TRAILING_ARG; then
+              HAVING_OPTIONS+=("$OPTION")
+              (( ARGI++ ))
+            fi
+          else
+            HAVING_OPTIONS+=("$OPTION")
+          fi
+  
+          HAVE_MATCH=true
+          break
+        fi
+      done
+  
+      if ! $HAVE_MATCH; then
+        local ARG_LENGTH=${#ARG}
+        local I=1
+        local IS_END=false
+        while ! $IS_END && test $I -lt $ARG_LENGTH; do
+          local ARG_CHAR="${ARG:$I:1}"
+          local HAVE_TRAILING_CHARS=$(test $((I+1)) -lt $ARG_LENGTH && echo true || echo false)
+  
+          for OPTION in $SHORT_OPTS_WITH_ARG $SHORT_OPTS_WITHOUT_ARG $SHORT_OPTS_WITH_OPTIONAL_ARG; do
+            local OPTION_CHAR="${OPTION:1:1}"
+  
+            if test "$ARG_CHAR" = "$OPTION_CHAR"; then
+              if $CONTAINS "$OPTION" "${SHORT_OPTS_WITH_ARG[@]}"; then
+                if $HAVE_TRAILING_CHARS; then
+                  HAVING_OPTIONS+=("$OPTION")
+                  IS_END=true
+                elif $HAVE_TRAILING_ARG; then
+                  HAVING_OPTIONS+=("$OPTION")
+                  (( ARGI++ ))
+                  IS_END=true
+                fi
+              elif $CONTAINS "$OPTION" "${SHORT_OPTS_WITH_OPTIONAL_ARG[@]}"; then
+                HAVING_OPTIONS+=("$OPTION")
+  
+                if $HAVE_TRAILING_CHARS; then
+                  IS_END=true
+                fi
+              else
+                HAVING_OPTIONS+=("$OPTION")
+              fi
+  
+              break
+            fi
+          done
+  
+          (( I++ ))
+        done
+      fi;;
+    (*)
+      POSITIONALS+=("$ARG");;
+  esac
+  
+  (( ARGI++ ))
 done
 
 if test $WANTED_POSITIONAL -eq 0; then
-echo "$FUNC: argv[2]: positionals start at 1, not 0!" >&2
-return 1
+  echo "$FUNC: argv[2]: positionals start at 1, not 0!" >&2
+  return 1
 fi
 
 printf "%s" "${POSITIONALS[$WANTED_POSITIONAL]}"
@@ -159,9 +159,9 @@ local -a DESCRIBE
 local LINE
 
 for LINE in "${OUTPUT_LINES[@]}"; do
-LINE="${LINE/:/\\:/}"
-LINE="${LINE/$'\t'/:}"
-DESCRIBE+=("$LINE")
+  LINE="${LINE/:/\\:/}"
+  LINE="${LINE/$'\t'/:}"
+  DESCRIBE+=("$LINE")
 done
 
 _describe '' DESCRIBE
