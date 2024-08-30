ed684d10d9d8c2f906b8f3821e4b7ec8673f4fd9
Benjamin Abendroth <braph93@gmx.de>
2024-08-30 10:01:17 +0200

    Small optimizations in alsa completions

--- completions/alsa/alsamixer.bash
+++ completions/alsa/alsamixer.bash
@@ -1,7 +1,7 @@
 _alsamixer_list_devices() {
   local card
   aplay -l | grep -Eo '^card [0-9]+: [^,]+' | uniq | while read card; do
-    card="${card##card }"
+    card="${card#card }"
     local id="${card%%: *}"
     echo "$id"
   done
