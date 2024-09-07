bbaf632bb1fbdcfba66621d8169c926e2e278fe3
Benjamin Abendroth <braph93@gmx.de>
2024-09-07 02:56:07 +0200

    Splitted test.py into utils.py

--- test/test.py
+++ test/test.py
@@ -5,100 +5,15 @@ import sys
 import time
 import threading
 import queue
-import subprocess
 
 import tests
+from   utils import *
 
 SHELLS = ['bash', 'fish', 'zsh']
 TMUX_SESSION_NAME = 'argparse-shell-complete-test'
 TESTS_OUTFILE = 'tests.new.py'
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-def run(args, env=None):
-    result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, env=env)
-
-    if result.returncode != 0:
-        raise Exception("Cmd %r failed: %s" % (args, result.stderr))
-
-    return result.stdout
-
-class TmuxClient:
-    def __init__(self, session):
-        self.session = session
-
-    def run(self, args):
-        env = os.environ.copy()
-        env.pop('TMUX', None)
-        return run(['tmux'] + args, env)
-
-    def new_session(self, command_args=[]):
-        self.run(['new-session', '-d', '-s', self.session] + command_args)
-
-    def kill_session(self):
-        self.run(['kill-session', '-t', self.session])
-
-    def send_keys(self, *keys):
-        self.run(['send-keys', '-t', self.session] + list(keys))
-
-    def capture_pane(self):
-        return self.run(['capture-pane', '-t', self.session, '-p'])
-
-    def resize_window(self, x, y):
-        self.run(['resize-window', '-t', self.session, '-x', str(x), '-y', str(y)])
-
-def start_shell(tmux, shell, completion_file):
-    if shell == 'bash':
-        cmd = ['bash', '--norc']
-        in_lines = '''\
-PS1='> '
-. /usr/share/bash-completion/bash_completion
-. %s
-''' % completion_file
-    elif shell == 'fish':
-        cmd = ['fish', '--no-config']
-        in_lines = '''\
-function fish_prompt; printf '> '; end
-source %s
-''' % completion_file
-    elif shell == 'zsh':
-        cmd = ['zsh', '--no-rcs']
-        in_lines = '''\
-PROMPT='> '
-autoload -U compinit && compinit
-source %s
-''' % completion_file
-    else:
-        raise Exception('Invalid shell: %r' % shell)
-
-    tmux.new_session(cmd)
-    tmux.resize_window(80, 100)
-    tmux.send_keys(in_lines)
-    time.sleep(0.5)
-
-def stop_shell(tmux):
-    tmux.kill_session()
-
-def complete(tmux, commandline, num_tabs=1):
-    # Clear screen
-    tmux.send_keys('C-c')
-    tmux.send_keys('clear')
-    tmux.send_keys('Enter')
-
-    time.sleep(0.3)
-
-    # Write commandline
-    tmux.send_keys(commandline)
-
-    # Write tabs
-    for i in range(num_tabs):
-        tmux.send_keys('Tab')
-
-    time.sleep(1)
-
-    result = tmux.capture_pane()
-    result = result.rstrip()
-    return result
 
 def generate_completion(shell, outfile, args):
     run(['../argparse-shell-complete', shell, '-o', outfile, 'argparse-shell-complete-test'] + args)
@@ -157,21 +72,28 @@ def add_empty_expected_to_tests():
 
 def do_tests(shell, result_queue):
     tmux = TmuxClient(TMUX_SESSION_NAME + '-' + shell)
+    tmux_shell = {'bash': BashShell, 'fish': FishShell, 'zsh': ZshShell}[shell](tmux)
 
     for test in tests.tests:
         test = test.copy()
         if 'generate-scripts' in test:
-            try:    stop_shell(tmux)
+            try:    tmux_shell.stop()
             except: pass
             completion_file = 'out.%s' % shell
             generate_completion(shell, completion_file, ['--zsh-compdef=False'] + test['generate-scripts'])
-            start_shell(tmux, shell, completion_file)
+            tmux_shell.start()
+            tmux.resize_window(80, 100)
+            tmux_shell.set_prompt()
+            tmux_shell.init_completion()
+            tmux_shell.load_completion(completion_file)
+            time.sleep(0.5)
+
         else:
             test[shell+'_result'] = complete(tmux, test['send'], test.get(shell+'_tabs', 1))
             result_queue.put(test)
 
     try:
-        stop_shell(tmux)
+        tmux_shell.stop()
     except:
         pass
 
